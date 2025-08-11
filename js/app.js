266b368b681d6c8390774cdf0eebc22514eb5c51
abdullatif <abdullatif>
2025-08-11 15:27:33 +0500

    vgvgre

--- js/app.js
+++ js/app.js
@@ -1,114 +1,140 @@
-// ================================
-//        OSON MASALALAR (7 ta)
-// ================================
+// // ================================
+// //        OSON MASALALAR (7 ta)
+// // ================================
 
-// 1-masala
-// Berilgan arrayga bitta qiymat qoвЂshing.
-// Foydalanish: push()
-const arrPush = [1, 2, 3, 4]
-function addToEnd(arr) {
-  arr.push(3);
-}
+// // 1-masala
+// // Berilgan arrayga bitta qiymat qoвЂshing.
+// // Foydalanish: push()
+// const arrPush = [1, 2, 3, 4]
+// function addToEnd(arr) {
+//   arr.push(3);
+// }
 
-addToEnd(arrPush);
-console.log(arrPush);
+// addToEnd(arrPush);
+// console.log(arrPush);
 
-// 2-masala
-// Arrayning oxirgi elementini olib tashlang.
-// Foydalanish: pop()
-const arrPop = [1, 2, 3, 4, 5];
-function removeLast(arr) {
-  arr.pop();
-}
+// // 2-masala
+// // Arrayning oxirgi elementini olib tashlang.
+// // Foydalanish: pop()
+// const arrPop = [1, 2, 3, 4, 5];
+// function removeLast(arr) {
+//   arr.pop();
+// }
 
-removeLast(arrPop);
-console.log(arrPop);
+// removeLast(arrPop);
+// console.log(arrPop);
 
-// 3-masala
-// Array boshiga yangi element qoвЂshing.
-// Foydalanish: unshift()
-const arrunshift = [1, 2, 3, 4];
-function addToStart(arr) {
-  arr.unshift(6);
-}
+// // 3-masala
+// // Array boshiga yangi element qoвЂshing.
+// // Foydalanish: unshift()
+// const arrunshift = [1, 2, 3, 4];
+// function addToStart(arr) {
+//   arr.unshift(6);
+// }
 
-addToStart(arrunshift);
-console.log(arrunshift);
+// addToStart(arrunshift);
+// console.log(arrunshift);
 
-// 4-masala
-// Arrayning birinchi elementini olib tashlang.
-// Foydalanish: shift()
-const arrshift = [1, 2, 3, 4];
-function removeFirst(arr) {
-  arr.shift("aslom");
-}
+// // 4-masala
+// // Arrayning birinchi elementini olib tashlang.
+// // Foydalanish: shift()
+// const arrshift = [1, 2, 3, 4];
+// function removeFirst(arr) {
+//   arr.shift("aslom");
+// }
 
-removeFirst(arrshift);
-console.log(arrshift);
+// removeFirst(arrshift);
+// console.log(arrshift);
 
-// 5-masala
-// Stringdagi boвЂsh joylarni olib tashlang.
-// Foydalanish: trim()
-const strtrim = "      Abdullatif     ";
-function cleanString(str) {
-  str.trim();
-}
+// // 5-masala
+// // Stringdagi boвЂsh joylarni olib tashlang.
+// // Foydalanish: trim()
+// const strtrim = "      Abdullatif     ";
+// function cleanString(str) {
+//   str.trim();
+// }
 
-cleanString(strtrim);
-console.log(strtrim);
+// cleanString(strtrim);
+// console.log(strtrim);
 
-// 6-masala
-// Berilgan string katta harflarga oвЂtkazilsin.
-// Foydalanish: toUpperCase()
-function shout(str) {
-  // Qichqiring! Lekin faqat kod bilan рџ™ѓ
-}
+// // 6-masala
+// // Berilgan string katta harflarga oвЂtkazilsin.
+// // Foydalanish: toUpperCase()
+// const strShout = "salom";
+// function shout(str) {
+//   str.toUpperCase();
+// }
 
-// 7-masala
-// Massivda qiymat bor-yoвЂqligini tekshiring.
-// Foydalanish: includes()
-function hasValue(arr, value) {
-  // Bor yoвЂq, tekshiring... lekin kod bilan рџ”Ќ
-}
+// shout(strShout);
+// console.log(strShout);
 
-// ================================
-//     OвЂRTACHA DARAJADAGI MASALALAR (7 ta)
-// ================================
+// // 7-masala
+// // Massivda qiymat bor-yoвЂqligini tekshiring.
+// // Foydalanish: includes()
+// const arrIncludes = "Abdullatif, Ali, Vali";
+// function hasValue(arr) {
+//   arr.includes("Abdullatifs");
+// }
+
+// hasValue(arrIncludes);
+// console.log(arrIncludes);
+
+// // ================================
+// //     OвЂRTACHA DARAJADAGI MASALALAR (7 ta)
+// // ================================
 
 // 8-masala
 // Berilgan soвЂz "a" harfi bilan boshlanishini tekshiring.
 // Foydalanish: startsWith()
+const strStartsWidth = "abdulla";
 function startsWithA(str) {
-  // Harflarning sardori kimligini aniqlang рџ‘‘
+  str.startsWith("a");
 }
+startsWithA(strStartsWidth);
+console.log(strStartsWidth);
 
 // 9-masala
 // Berilgan array ichida "apple" borligini aniqlang.
 // Foydalanish: includes()
+const arrIncludes = ["olma", "shaftoli", "banan"];
 function containsApple(arr) {
-  // Meva ichidan olmani toping рџЌЋ
+  arr.includes("olma");
 }
+containsApple(arrIncludes);
+console.log(arrIncludes);
 
 // 10-masala
 // Object ichidagi barcha kalitlarni arrayga aylantiring.
 // Foydalanish: Object.keys()
+const objObjectkeys = {
+  name: "Ali",
+  age: 20,
+};
 function getKeys(obj) {
-  // Kalitni toping, eshikni oching! рџ—ќпёЏ
+  return Object.keys(objObjectkeys);
 }
+
+const keysArray = getKeys(objObjectkeys);
+console.log(keysArray);
 
 // 11-masala
 // Object ichidagi barcha qiymatlarni arrayga oвЂtkazing.
 // Foydalanish: Object.values()
+const objObjectkeys11 = {
+  name: "Ali",
+  age: 21,
+};
 function getValues(obj) {
-  // Qiymat izlaymiz рџ’ё
+  return Object.values(objObjectkeys);
 }
+
+const keysArray11 = getValues(objObjectkeys);
+console.log(keysArray);
 
 // 12-masala
 // String ichidagi barcha boвЂsh joylarni olib tashlang.
 // Foydalanish: replaceAll(" ", "")
-function removeSpaces(str) {
-  // Joy yoвЂq, harf koвЂp! рџ”Ў
-}
+function removeSpaces(str) {}
 
 // 13-masala
 // Arrayning teskari koвЂrinishini qaytaring.
@@ -142,12 +168,6 @@ function lastThree(str) {
   // Oxirgi uchlikni e'tiborsiz qoldirmang рџЋЇ
 }
 
-// 17-masala
-// Object.entries() + fromEntries() orqali object ichidagi "age" qiymatiga 1 qoвЂshing.
-function increaseAge(obj) {
-  // Yosh yashiringan, toping va koвЂpaytiring рџ‘¶вћ•
-}
-
 // 18-masala
 // Stringni ichidagi har bir boвЂshliqdan keyin vergul qoвЂshing.
 // Masalan: "salom dunyo" => "salom, dunyo"
@@ -173,5 +193,5 @@ function sliceMiddle(arr) {
 // String ichida "javascript" soвЂzini "JS" bilan almashtiring.
 // Foydalanish: replaceAll()
 function shortenJS(str) {
-  // Uzun soвЂzlardan qutulamiz, qisqacha ayting рџ§ 
+  // Uzun soвЂzlardan qutulamiz, qisqacha ayting рџ§
 }
